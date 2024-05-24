098e94f624249c43f90f355bfa33842073db4011
olesien <olesien2@gmail.com>
2024-05-24 14:17:45 +0200

    Added ability to add task

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function AddForm({ close }) {
+export default function AddForm({ close, submit }) {
+    const [content, setContent] = useState("");
+    const [type, setType] = useState("ux");
     const clickedForm = (e) => {
         e.stopPropagation();
     };
+
     return (
         <div id="overlay" onClick={() => close()}>
-            <div className="overlay-form" onClick={clickedForm}>
+            <div className="overlay-content" onClick={clickedForm}>
                 <div className="close-overlay">
                     <p onClick={() => close()}>X</p>
                 </div>
                 <h3>Add Task</h3>
+                <form
+                    method="post"
+                    onSubmit={(e) => submit(e, content, type)}
+                    className="overlay-form"
+                >
+                    <label>
+                        <span>Content</span>
+                        <input
+                            value={content}
+                            onChange={(v) => setContent(v.target.value)}
+                            type="text"
+                            name="content"
+                            id="content"
+                            placeholder="fix everything"
+                        />
+                    </label>
+                    <label>
+                        <span>Type</span>
+                        <select
+                            name="type"
+                            id="type"
+                            value={type}
+                            onChange={(v) => setType(v.target.value)}
+                        >
+                            <option value="ux">UX</option>
+                            <option value="devbackend">Dev Backend</option>
+                            <option value="devfrontend">Dev Frontend</option>
+                            <option value="bug">Bug</option>
+                        </select>
+                    </label>
+                    <button type="Add">Submit</button>
+                </form>
             </div>
         </div>
     );
--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -3,10 +3,20 @@ import AddForm from "./AddForm";
 
 export default function AddToDo({ addTask }) {
     const [showForm, setShowForm] = useState(false);
+
+    const submit = (e, content, type) => {
+        console.log(e);
+        e.preventDefault();
+        console.log(content, type);
+        addTask({ content, type });
+        setShowForm(false);
+    };
     return (
         <div>
             <button onClick={() => setShowForm(true)}>Add Task</button>
-            {showForm && <AddForm close={() => setShowForm(false)} />}
+            {showForm && (
+                <AddForm close={() => setShowForm(false)} submit={submit} />
+            )}
         </div>
     );
 }
